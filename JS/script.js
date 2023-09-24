669d34b71a2acc09702ac04bb26ca42f669259ea
Akaina1 <erinadost@gmail.com>
2023-09-24 13:20:22 -0400

    creating Nav button animation

--- JS/script.js
+++ JS/script.js
@@ -2,8 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
     const navButton = document.querySelector('.nav__button');
     navButton.classList.add('nav__button--initial');
 
-    // Trigger reflow to restart the animation
-    void navButton.offsetWidth;
+    // Remove the initial class on hover to trigger hover effect
+    navButton.addEventListener('mouseover', function () {
+        navButton.classList.remove('nav__button--initial');
+    });
 
-    navButton.classList.remove('nav__button--initial');
+    // Re-add the initial class when the mouse moves out to restart the animation
+    navButton.addEventListener('mouseout', function () {
+        navButton.classList.add('nav__button--initial');
+    });
 });
+
