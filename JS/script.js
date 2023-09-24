dd96baf52bf5484df2a90721d5a7437e3bd3f041
Akaina1 <erinadost@gmail.com>
2023-09-24 15:18:47 -0400

    editing menu navigation

--- JS/script.js
+++ JS/script.js
@@ -1,36 +1,76 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Event Listeners
 document.addEventListener("DOMContentLoaded", function () {
+    // Initial Animation for navButton
     const navButton = document.querySelector('.nav__button');
     navButton.classList.add('nav__button--initial');
 
-    // Remove the initial class on hover to trigger hover effect
     navButton.addEventListener('mouseover', function () {
         navButton.classList.remove('nav__button--initial');
     });
 
-    // Re-add the initial class when the mouse moves out to restart the animation
     navButton.addEventListener('mouseout', function () {
-        navButton.classList.add('nav__button--initial');
+        if (submenu.style.display === 'none' || submenu.style.display === '') {
+            navButton.classList.add('nav__button--initial');
+        }
     });
-});
 
-document.addEventListener('DOMContentLoaded', function () {
-    const title = document.getElementById('homeTitle');
-    const text = title.textContent;
-    title.innerHTML = '';
+    // Submenu Display for mainButton
+    const mainButton = document.getElementById('mainButton');
+    const submenu = document.querySelector('.nav__submenu');
+    const mainButtonText = mainButton.querySelector('.nav__button-text');
+
+    mainButton.addEventListener('click', function () {
+        if (submenu.style.display === 'none' || submenu.style.display === '') {
+            submenu.style.display = 'block';
+            mainButton.classList.remove('nav__button--initial');
+            mainButton.classList.add('nav__button--activated');
+
+            // Update text style when activated
+            mainButtonText.style.color = 'var(--primary-text-color)';
+            mainButtonText.style.fontFamily = "'Nanum Gothic Coding', cursive";
+            mainButtonText.style.fontSize = '32px';
+
+            submenu.classList.add('show');
+        } else {
+            submenu.style.display = 'none';
+            mainButton.classList.remove('nav__button--activated');
+            mainButton.classList.add('nav__button--initial');
+
+            // Revert text style when deactivated
+            mainButtonText.style.color = 'transparent';
+            mainButtonText.style.fontFamily = ''; // revert to whatever the default was
+            mainButtonText.style.fontSize = '20px'; // revert to original size
+
+            submenu.classList.remove('show');
+        }
+    });
+
+    // Text Animation for page titles
+    const pageTitles = document.querySelectorAll('.page__title');
+    pageTitles.forEach(animateTitle); // Assuming you've defined animateTitle elsewhere
+});
+////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Functions
+function animateTitle(titleElement) {
+    const text = titleElement.textContent;
+    titleElement.innerHTML = '';
 
     for (let i = 0; i < text.length; i++) {
         let character = text[i];
         let span = document.createElement('span');
         span.className = 'character';
-        span.textContent = character === ' ' ? '\u00A0' : character;  // Replace ' ' with a non-breaking space
-        title.appendChild(span);
+        span.textContent = character === ' ' ? '\u00A0' : character; // Replace ' ' with a non-breaking space
+        titleElement.appendChild(span);
     }
 
-    const characters = document.querySelectorAll('.character');
+    const characters = titleElement.querySelectorAll('.character');
     characters.forEach((character, index) => {
         setTimeout(() => {
             character.style.opacity = '1';
             character.style.transform = 'translateY(0)';
-        }, index * 150);  // 100ms delay between each
+        }, index * 100); // 100ms delay between each
     });
-});
+};
